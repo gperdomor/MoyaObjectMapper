441e6fa2bc1d6327a04ff3165476b8e93c78dc8d
Gustavo Perdomo <gperdomor@gmail.com>
2017-02-19 02:07:39 -0300

    Init

--- Package.swift
+++ Package.swift
@@ -1,5 +1,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "MoyaModelMapper"
+    name: "MoyaModelMapper",
+    targets: [
+        Target(name: "MoyaModelMapper"),
+        Target(name: "RxMoyaModelMapper", dependencies: ["MoyaModelMapper"]),
+        Target(name: "ReactiveMoyaModelMapper", dependencies: ["MoyaModelMapper"])
+    ],
+    dependencies: [
+        .Package(url: "https://github.com/Moya/Moya", majorVersion: 8),
+        .Package(url: "https://github.com/lyft/mapper", majorVersion: 6)
+    ]
 )
